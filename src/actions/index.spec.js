e1966538ceb267246cf2adabe9d37b266e263557
Hanna Söderström <gothbarbie84@gmail.com>
2018-05-28 18:49:58 +0200

    Add test for #fetchRecentlies

---
+++ src/actions/index.spec.js
@@ -0,0 +1,89 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+import { SET_RECENTLIES, LOADING } from './types'
+import { fetchRecentlies, fetchRecently } from './index'
+
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+
+const axiosMock = new MockAdapter(axios)
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+// const mockResponse = (status, statusText, response) => {
+//   return new window.Response(response, {
+//     status,
+//     statusText,
+//     headers: {
+//       'Content-type': 'application/json',
+//     },
+//   })
+// }
+
+describe('actions', () => {
+  beforeEach(() => {
+    axiosMock
+      .onGet('http://hannasoderstrom.com/admin/wp-json/wp/v2/posts')
+      .reply(200, [
+        {
+          id: 1,
+          date: '2016-04-13T12:17:09',
+          slug: 'number-one',
+          title: { rendered: 'Number One' },
+          excerpt: { rendered: '<p>Some text about stuff.</p>' },
+          content: { rendered: '<p>Some text about stuff.</p>' },
+        },
+        {
+          id: 2,
+          date: '2016-04-14T12:17:09',
+          slug: 'number-two',
+          title: { rendered: 'Number Two' },
+          excerpt: { rendered: '<p>Some other text about stuff.</p>' },
+          content: { rendered: '<p>Some other text about stuff.</p>' },
+        },
+      ])
+  })
+
+  afterEach(() => {
+    axiosMock.reset()
+  })
+  describe('#fetchRecentlies', () => {
+    it('returns recentlies from the CMS', () => {
+      const store = mockStore()
+      const recentlies = {
+        payload: [
+          {
+            id: 1,
+            date: '2016-04-13T12:17:09',
+            slug: 'number-one',
+            title: { rendered: 'Number One' },
+            excerpt: { rendered: '<p>Some text about stuff.</p>' },
+            content: { rendered: '<p>Some text about stuff.</p>' },
+          },
+          {
+            id: 2,
+            date: '2016-04-14T12:17:09',
+            slug: 'number-two',
+            title: { rendered: 'Number Two' },
+            excerpt: { rendered: '<p>Some other text about stuff.</p>' },
+            content: { rendered: '<p>Some other text about stuff.</p>' },
+          },
+        ],
+        type: SET_RECENTLIES,
+      }
+
+      // window.axios = jest
+      //   .fn()
+      //   .mockImplementation(() =>
+      //     Promise.resolve(mockResponse(200, null, recentlies))
+      //   )
+
+      return store.dispatch(fetchRecentlies()).then(() => {
+        const expectedActions = store.getActions()
+        expect(expectedActions[1]).toEqual(recentlies)
+      })
+    })
+  })
+})
