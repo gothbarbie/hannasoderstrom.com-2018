bfb82b5548308dc054c4c24c12167e28e7a5d032
Hanna Söderström <gothbarbie84@gmail.com>
2018-05-29 09:41:49 +0200

    finish test for #fetchRecently

--- src/actions/index.spec.js
+++ src/actions/index.spec.js
@@ -12,44 +12,34 @@ const axiosMock = new MockAdapter(axios)
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
-// const mockResponse = (status, statusText, response) => {
-//   return new window.Response(response, {
-//     status,
-//     statusText,
-//     headers: {
-//       'Content-type': 'application/json',
-//     },
-//   })
-// }
-
 describe('actions', () => {
-  beforeEach(() => {
-    axiosMock
-      .onGet('http://hannasoderstrom.com/admin/wp-json/wp/v2/posts')
-      .reply(200, [
-        {
-          id: 1,
-          date: '2016-04-13T12:17:09',
-          slug: 'number-one',
-          title: { rendered: 'Number One' },
-          excerpt: { rendered: '<p>Some text about stuff.</p>' },
-          content: { rendered: '<p>Some text about stuff.</p>' },
-        },
-        {
-          id: 2,
-          date: '2016-04-14T12:17:09',
-          slug: 'number-two',
-          title: { rendered: 'Number Two' },
-          excerpt: { rendered: '<p>Some other text about stuff.</p>' },
-          content: { rendered: '<p>Some other text about stuff.</p>' },
-        },
-      ])
-  })
-
-  afterEach(() => {
-    axiosMock.reset()
-  })
   describe('#fetchRecentlies', () => {
+    beforeEach(() => {
+      axiosMock
+        .onGet('http://hannasoderstrom.com/admin/wp-json/wp/v2/posts')
+        .reply(200, [
+          {
+            id: 1,
+            date: '2016-04-13T12:17:09',
+            slug: 'number-one',
+            title: { rendered: 'Number One' },
+            excerpt: { rendered: '<p>Some text about stuff.</p>' },
+            content: { rendered: '<p>Some text about stuff.</p>' },
+          },
+          {
+            id: 2,
+            date: '2016-04-14T12:17:09',
+            slug: 'number-two',
+            title: { rendered: 'Number Two' },
+            excerpt: { rendered: '<p>Some other text about stuff.</p>' },
+            content: { rendered: '<p>Some other text about stuff.</p>' },
+          },
+        ])
+    })
+
+    afterEach(() => {
+      axiosMock.reset()
+    })
     it('returns recentlies from the CMS', () => {
       const store = mockStore()
       const recentlies = {
@@ -74,15 +64,54 @@ describe('actions', () => {
         type: SET_RECENTLIES,
       }
 
-      // window.axios = jest
-      //   .fn()
-      //   .mockImplementation(() =>
-      //     Promise.resolve(mockResponse(200, null, recentlies))
-      //   )
-
       return store.dispatch(fetchRecentlies()).then(() => {
         const expectedActions = store.getActions()
         expect(expectedActions[1]).toEqual(recentlies)
+      })
+    })
+  })
+
+  describe('#fetchRecently', () => {
+    beforeEach(() => {
+      axiosMock
+        .onGet(
+          'http://hannasoderstrom.com/admin/wp-json/wp/v2/posts?slug=number-two'
+        )
+        .reply(200, [
+          {
+            id: 2,
+            date: '2016-04-14T12:17:09',
+            slug: 'number-two',
+            title: { rendered: 'Number Two' },
+            excerpt: { rendered: '<p>Some other text about stuff.</p>' },
+            content: { rendered: '<p>Some other text about stuff.</p>' },
+          },
+        ])
+    })
+
+    afterEach(() => {
+      axiosMock.reset()
+    })
+
+    it('returns specific recently by slug', () => {
+      const store = mockStore()
+      const recently = {
+        payload: [
+          {
+            id: 2,
+            date: '2016-04-14T12:17:09',
+            slug: 'number-two',
+            title: { rendered: 'Number Two' },
+            excerpt: { rendered: '<p>Some other text about stuff.</p>' },
+            content: { rendered: '<p>Some other text about stuff.</p>' },
+          },
+        ],
+        type: SET_RECENTLIES,
+      }
+
+      return store.dispatch(fetchRecently('number-two')).then(() => {
+        const expectedActions = store.getActions()
+        expect(expectedActions[1]).toEqual(recently)
       })
     })
   })
