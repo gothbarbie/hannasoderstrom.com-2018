8c51e037ed485313236ba1078a061a884352ad55
Hanna Söderström <gothbarbie84@gmail.com>
2018-08-01 16:35:54 +0200

    style: change font size for different screen sizes

--- src/components/Typography/P/P.js
+++ src/components/Typography/P/P.js
@@ -1,11 +1,21 @@
 import styled from 'styled-components'
 
 const P = styled.p`
-  font-size: 1.5rem;
-  line-height: 2rem;
   margin: 1rem 0;
   font-weight: 200;
   color: #333;
+  font-size: 1rem;
+  line-height: 1.5rem;
+
+  @media (min-width: 768px) {
+    font-size: 1.25rem;
+    line-height: 2rem;
+  }
+
+  @media (min-width: 1040px) {
+    font-size: 1.5rem;
+    line-height: 2rem;
+  }
 `
 
 export default P
