cdc761ca6fe78445815f66e087679ce87102ed43
Hanna Söderström <gothbarbie84@gmail.com>
2018-08-09 17:15:22 +0200

    style: fix margins for different sizes

--- src/components/Typography/P/P.js
+++ src/components/Typography/P/P.js
@@ -1,18 +1,20 @@
 import styled from 'styled-components'
 
 const P = styled.p`
-  margin: 1rem 0 2rem;
+  margin: 0 0 1rem 0;
   font-weight: 200;
   color: #333;
   font-size: 1rem;
   line-height: 1.5rem;
 
   @media (min-width: 768px) {
+    margin: 0 0 2rem;
     font-size: 1.25rem;
     line-height: 2rem;
   }
 
   @media (min-width: 1040px) {
+    margin: 0 0 2rem;
     font-size: 1.5rem;
     line-height: 2rem;
   }
